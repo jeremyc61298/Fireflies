1bdea0818bba36cb880f845e2ab5b8a78cecef3b
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-06 22:32:03 -0600

    Init commit

---
+++ src/firefly.java
@@ -0,0 +1,16 @@
+// -----------------------------------------------
+// firefly.java
+// Jeremy Campbell
+// Computation geometry problem for Applied
+// -----------------------------------------------
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class firefly {
+
+    public static void main(String[] args) throws IOException {
+        Scanner fin = new Scanner("firefly.in");
+        PrintWriter fout = new PrintWriter("firefly.out");
+    }
+}
