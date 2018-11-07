5712ad4d2861c98227250dfc10f462015f7cd83e
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-07 13:21:36 -0600

    Init ChaseScenario, which reads a scenario from file.

--- src/firefly.java
+++ src/firefly.java
@@ -5,6 +5,10 @@
 // -----------------------------------------------
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class firefly {
@@ -12,21 +16,36 @@ public class firefly {
     private static class Point {
         double x;
         double y;
+
+        public Point() {}
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 
     private static class ChaseScenario {
 
         private int maxDistance;
+        private Point chaserLocation;
+        private List<Point> fireflyLocations = new LinkedList<>();
 
         public boolean readChaseScenario(Scanner fin) {
             maxDistance = fin.nextInt();
             if (maxDistance == 0) {
                 return false;
-            } else {
-
             }
+            chaserLocation.x = fin.nextInt();
+            chaserLocation.y = fin.nextInt();
+
+            Point temp = new Point(fin.nextInt(), fin.nextInt());
+            while (temp.x != -1 && temp.y != -1) {
+                fireflyLocations.add(temp);
+                temp = new Point(fin.nextInt(), fin.nextInt());
+            }
+            return true;
         }
-        return true;
     }
 
 
@@ -35,7 +54,7 @@ public class firefly {
         PrintWriter fout = new PrintWriter("firefly.out");
 
         ChaseScenario cs = new ChaseScenario();
-        while (cs.readChaseScenario(fin) != false) {
+        while (cs.readChaseScenario(fin)) {
 
         }
     }
