8e6f3e0bf29f6302ea64d2c46393d3322aeb4779
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-07 19:21:33 -0600

    Passes all test cases given by Dr. Baber

--- src/firefly.java
+++ src/firefly.java
@@ -3,11 +3,13 @@
 // Jeremy Campbell
 // Computation geometry problem for Applied
 // -----------------------------------------------
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.lang.Double;
 
 public class firefly {
 
@@ -24,7 +26,7 @@ public class firefly {
 
         @Override
         public String toString() {
-            return "(" + this.x.toString() + this.y.toString() + ")";
+            return "(" + this.x.intValue() + "," + this.y.intValue() + ")";
         }
     }
 
@@ -47,9 +49,29 @@ public class firefly {
         }
 
         public Point catchFirefly() {
-            // TODO: Finish this function using the distance and slope formula's
+            // TODO: Finish this function using the distance and slope formulas
+            for (Point p : fireflyLocations) {
+                double distance = distanceToFirefly(p);
+                if (distance <= (maxDistance + 1)) {
+                    // Firefly will be caught
+                    return p;
+                } else {
+                    // Firefly will not be caught, chaser teleports closer
+                    chaserLocation.x = buildClosestPoint(distance, chaserLocation.x, p.x);
+                    chaserLocation.y = buildClosestPoint(distance, chaserLocation.y, p.y);
+                }
+            }
+            // Firefly was never caught
+            return null;
+        }
 
-            return new Point(-1, -1);
+        private double distanceToFirefly(Point p) {
+            return Math.sqrt(Math.pow(p.x - chaserLocation.x, 2)
+                            + Math.pow(p.y - chaserLocation.y, 2));
+        }
+
+        private double buildClosestPoint(double d, double c, double f) {
+            return ((maxDistance * (f - c)) / d) + c;
         }
 
         public void reset() {
@@ -59,18 +81,26 @@ public class firefly {
 
 
     public static void main(String[] args) throws IOException {
-        Scanner fin = new Scanner("firefly.in");
+        Scanner fin = new Scanner(new File("firefly.in"));
         PrintWriter fout = new PrintWriter("firefly.out");
 
         ChaseScenario cs = new ChaseScenario();
         double maxDistance = fin.nextDouble();
+        int fireflyNum = 1;
+
         while (maxDistance != 0) {
             cs.readChaseScenario(fin, maxDistance);
             Point captureLocation = cs.catchFirefly();
-            fout.print(captureLocation.toString());
-            cs.reset();
 
-            // Print the endline is there is another scenario
+            if (captureLocation != null)
+                fout.print("Firefly " + fireflyNum + " caught at " + captureLocation.toString());
+            else
+                fout.print("Firefly " + fireflyNum + " not caught");
+
+            cs.reset();
+            fireflyNum++;
+
+            // Print the endline if there is another scenario
             maxDistance = fin.nextDouble();
             if (maxDistance != 0)
                 fout.println();
